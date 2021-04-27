8cb61667fe263e05201cfbcead9a015b93695ee2
AMU <aamouie01@gmail.com>
2021-04-27 20:55:18 +0430

    Update scales.cpp

--- scales.cpp
+++ scales.cpp
@@ -1,15 +1,18 @@
 // This piece of code is made to make it easier for me when
 // I start new projects and want to use notes on scales.
 
+int minMidiNoteValue = 36; // C2
+int maxMidiNoteValue = 88; // C8
+int octaveNumbers = 3; // 2 -> 4
+
+int majorScaleNotes [7] = {};
+int minorScaleNotes [7] = {};
+
+string majorScaleChords [7] = {};
+string minorScaleChords [7] = {};
+
 void GIMME_MY_SCALES()
 {
-    int minMidiNoteValue = 36; // C2
-    int maxMidiNoteValue = 88; // C8
-    int octaveNumbers = 3; // 2 -> 4
-
-    int majorScaleNotes [7] = {};
-    int minorScaleNotes [7] = {};
-
     majorScaleNotes[0] = 0;
     majorScaleNotes[1] = 2;
     majorScaleNotes[2] = 4;
@@ -25,4 +28,27 @@ void GIMME_MY_SCALES()
     minorScaleNotes[4] = 7;
     minorScaleNotes[5] = 8;
     minorScaleNotes[6] = 10;
+}
+
+void GIMME_MY_CHORDS()
+{
+    minorScaleChords[0] = "m";
+    minorScaleChords[1] = "dim";
+    minorScaleChords[2] = "M";
+    minorScaleChords[3] = "m";
+    minorScaleChords[4] = "m";
+    minorScaleChords[5] = "M";
+    minorScaleChords[6] = "M";
+    
+    majorScaleChords[0] = "M";
+    majorScaleChords[1] = "m";
+    majorScaleChords[2] = "m";
+    majorScaleChords[3] = "M";
+    majorScaleChords[4] = "M";
+    majorScaleChords[5] = "m";
+    majorScaleChords[6] = "dim";
+    
+    int majorChord [3] = {'1','3','5'};
+    int minorChord [3] = {'1','3b','5'};
+    int dimChord [3] = {'1','3b','5b'};
 }
